5289e0db14ac2e5740851f0a880079a7a69c6cff
AJ Villalobos <hi@ajvillalobos.com>
2018-04-18 15:52:01 +0800

    Read bitcoin status from database

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,17 @@
 class DashboardsController < ApplicationController
   def show
+    @exchange_rate = ExchangeRate.last
+    @status = status
+  end
+
+  private
+
+  def status
+    return "NO!" if down?
+    "YES"
+  end
+
+  def down?
+    @exchange_rate.percent_change_24h.include?("-")
   end
 end
